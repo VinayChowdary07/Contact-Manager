dbe7c8e16acc17f6d88d48cb3f6b9ba495ac5c35
Vinay Chowdary <vinayjustin322@gmail.com>
2025-05-03 14:05:17 +0530

    Update db_config.py for Railway deployment

--- db_config.py
+++ db_config.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Vinay@8585",
-        database="contact_manager"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
